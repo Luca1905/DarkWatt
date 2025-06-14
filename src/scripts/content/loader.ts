80c96c67228c94f2a09667699ed0b3eb155ba2a6
Luca1905 <contact@lucawang.me>
2025-06-14 15:15:12 +0200

    MISC: seperate folders

--- src/scripts/content/loader.ts
+++ src/scripts/content/loader.ts
@@ -1,6 +1,7 @@
 const script: HTMLScriptElement = document.createElement('script');
-script.src = chrome.runtime.getURL('script.js');
+script.src = chrome.runtime.getURL('content/script.js');
 (document.head || document.documentElement).appendChild(script);
 script.onload = function () {
   script.remove();
 };
+
