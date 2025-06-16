1f8ce4e08c94e67d36eb0bfee60c39c84322f7ea
Luca1905 <contact@lucawang.me>
2025-06-16 21:03:09 +0200

    DEMO working + UI scaffhold

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,9 +1,6 @@
 import initWasmModule, { hello_wasm } from './wasm/wasm_mod.js';
 
-console.log("Background script started");
-console.log(await chrome.permissions.getAll());
-
 (async () => {
-  await initWasmModule();
-  hello_wasm();
+    await initWasmModule();
+    hello_wasm();
 })();
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -2,6 +2,4 @@ console.log("popup loaded");
 
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("Toolbar button clicked");
-  const mainElement = document.getElementById("main");
-  mainElement.textContent = "popup js working";
 });
